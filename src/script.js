c0a61bdb554c672ebbf719ed21962d9c523bc11f
mueni <charlesmueni2@gmail.com>
2023-10-25 19:56:30 +0300

    changed project

--- src/script.js
+++ src/script.js
@@ -1,49 +1,45 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    
-// Select DOM elements
-const apodImage = document.getElementById('apod-image');
-const apodTitle = document.getElementById('apod-title');
-const apodExplanation = document.getElementById('apod-explanation');
-const apodDate = document.getElementById('apod-date');
-const likeButton = document.getElementById('like-button');
-const likeCount = document.getElementById('like-count');
-const commentForm = document.getElementById('comment-form');
-const commentList = document.getElementById('comment-list');
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("searchForm");
+  const resultContainer = document.getElementById("resultContainer");
 
-// Fetch Astronomy Picture of the Day from API
-fetch('https://api.nasa.gov/planetary/apod?api_key=neIf881jkrVVBuY8LMMlAKE1DgCukaoc0JS6GZoU')
-  .then(response => response.json())
-  .then(data => {
+  form.addEventListener("submit", async function (e) {
+      e.preventDefault();
 
-    // Set image source, title, explanation and date
-    apodImage.src = data.hdurl;
-    apodTitle.textContent = data.title;
-    apodExplanation.textContent = data.explanation;
-    apodDate.textContent = data.date;
-  })
-  .catch(error => console.error(error));
+      // Get the user's input from the form
+      const searchInput = document.getElementById("searchInput").value;
 
-  // Add event listener to like button
-likeButton.addEventListener('click', () => {
-  let count = parseInt(likeCount.textContent);
-  count++;
-  likeCount.textContent = count;
+      // Check if the search input is not empty
+      if (!searchInput) {
+          resultContainer.innerHTML = "Please enter a search query.";
+          return;
+      }
+
+      // Construct the API URL with the user's input as a query parameter
+      const apiUrl = `https://google-search-json.p.rapidapi.com/search/web?q=${encodeURIComponent(searchInput)}`;
+      const options = {
+          method: "GET",
+          headers: {
+              "X-RapidAPI-Key": "9ae39133ffmsh3cfa88e47b748cap1d27a5jsn3e767274536f",
+              "X-RapidAPI-Host": "google-search-json.p.rapidapi.com",
+          },
+      };
+
+      try {
+          const response = await fetch(apiUrl, options);
+          const data = await response.json(); // Parse JSON data
+
+          // Clear previous results
+          resultContainer.innerHTML = '';
+
+          // Loop through the search results and display them
+          data.items.forEach(item => {
+              const title = item.title;
+              const link = item.link;
+              resultContainer.innerHTML += `<p><a href="${link}" target="_blank">${title}</a></p>`;
+          });
+      } catch (error) {
+          console.error(error);
+          resultContainer.innerHTML = "An error occurred while fetching data.";
+      }
+  });
 });
-
-// Add event listener to comment form
-commentForm.addEventListener('submit', event => {
-  event.preventDefault();
-  const nameInput = document.getElementById('name-input');
-  const commentInput = document.getElementById('comment-input');
-  const name = nameInput.value.trim();
-  const comment = commentInput.value.trim();
-  if (name && comment) {
-    const commentItem = document.createElement('li');
-    commentItem.innerHTML = `<strong>${name}</strong>: ${comment}`;
-    commentList.appendChild(commentItem);
-    nameInput.value = '';
-    commentInput.value = '';
-  }
-});
-
-})
